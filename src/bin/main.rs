94b87191a012eff31191cf135d0d4fac49f61fa7
Philippe Nadon <phil@nadon.io>
2020-07-27 14:23:05 -0600

    basic worker pool working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,19 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
+use hello_tcp::ThreadPool;
 use std::fs;
 
 static ADDRESS: &str = "127.0.0.1:7878";
 
 fn main() {
     let listener = TcpListener::bind(ADDRESS).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::sync::mpsc;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Creates a new ThreadPool
+    /// 
+    /// size: number of threads in the pool
+    /// 
+    /// # Panics
+    /// 
+    /// Panics if the size is 0
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender}
+    }
+
+    /// Sends the task `f` through a channel to an awaiting Worker
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} received job, executing...", id);
+
+            job();
+        });
+
+        Worker { id, thread}
+    }
+}
